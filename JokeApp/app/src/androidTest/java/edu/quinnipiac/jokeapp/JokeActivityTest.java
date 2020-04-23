9303f79aa82838c5f087301751faa4b94077b9fc
jsaleh8250 <54547992+jsaleh8250@users.noreply.github.com>
2020-04-23 00:06:56 -0400

    Add files via upload

---
+++ JokeApp/app/src/androidTest/java/edu/quinnipiac/jokeapp/JokeActivityTest.java
@@ -0,0 +1,34 @@
+package edu.quinnipiac.ser210.jokeapplication;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class JokeActivityTest {
+    @Rule
+    // public ActivityTestRule<JokeActivity> jokeActivityActivityTestRule =
+    // new ActivityTestRule<JokeActivity>(JokeActivity.class);
+    private JokeActivity jokeActivity = null;
+
+    @Test
+    public void onCreate() {
+        //jokeActivity = jokeActivityActivityTestRule.getActivity();
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+
+        fail("Not yet implemented");
+    }
+}
---
+++ JokeApp/app/src/androidTest/java/edu/quinnipiac/jokeapp/ResultsHandlerTest.java
@@ -0,0 +1,29 @@
+package edu.quinnipiac.ser210.jokeapplication;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.fail;
+
+@RunWith(AndroidJUnit4.class)
+public class ResultsHandlerTest {
+    @Rule
+   // public ActivityTestRule<ResultsHandler> resultsHandlerActivityTestRule =
+           // new ActivityTestRule<ResultsHandler>(ResultsHandler.class);
+    private ResultsHandler resultsHandler = null;
+    @Before
+    public void setUp() throws Exception {
+        //ResultsHandler = ResultsHandlerActivityTestRule.getActivity();
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getJokeInfo() {
+        fail("Not produced");
+    }
+}
