ba3822d8f69b717ae2cd80ea3fdbb0c2db006bbd
jsaleh8250 <54547992+jsaleh8250@users.noreply.github.com>
2020-05-02 17:28:38 -0400

    comments

--- app/src/main/java/edu/quinnipiac/ser210/jokeapplication/CreatedJokes.java
+++ app/src/main/java/edu/quinnipiac/ser210/jokeapplication/CreatedJokes.java
@@ -1,26 +1,42 @@
 package edu.quinnipiac.ser210.jokeapplication;
 
+/*
+@authors: Victoria Gorski, Jenna Saleh, and Julia Wilkinson
+@date: 5 - 2 - 20
+@description: The CreatedJokes class is used to store the user's created joke and place it into the database.
+ */
+
+// Start of class
 public class CreatedJokes {
+
+    // Instance variables
     private long id;
     private String joke;
 
 
+    // Methods
+    // Get the ID of the joke
     public long getId() {
         return id;
     }
 
+    // Set the ID of the joke
     public void setId(long id) {
         this.id = id;
     }
+
+    // Set the joke into a string
     public void setJoke(String joke) {
         this.joke = joke;
     }
+
+    // Get the joke
     public String getJoke() {
         return joke;
     }
 
+    // Set the created joke
     public CreatedJokes(String joke) {
         this.joke = joke;
     }
-
 }
