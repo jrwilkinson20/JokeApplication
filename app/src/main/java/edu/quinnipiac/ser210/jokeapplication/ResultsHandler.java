93019b98db60875497b28f668f3bc98ad6c76c1e
jsaleh8250 <54547992+jsaleh8250@users.noreply.github.com>
2020-05-02 17:32:42 -0400

    comments

--- app/src/main/java/edu/quinnipiac/ser210/jokeapplication/ResultsHandler.java
+++ app/src/main/java/edu/quinnipiac/ser210/jokeapplication/ResultsHandler.java
@@ -1,35 +1,38 @@
 package edu.quinnipiac.ser210.jokeapplication;
 
-import android.graphics.Color;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
+/*
+@authors: Victoria Gorski, Jenna Saleh, and Julia Wilkinson
+@date: 5 - 2 - 20
+@description: The ResultsHandler class handles the results retrieve from the API.
+ */
 
+// Imports
 import org.json.JSONException;
 import org.json.JSONObject;
 
+// Start of class
 public class ResultsHandler {
-    public ResultsHandler(){
-
+    public ResultsHandler() {
     }
+
+    // Methods
+    // Get the joke information from the API
     public String getJokeInfo(String jokeInfo) throws JSONException {
         String category, joke, punchline;
         String printString;
         JSONObject jokeData = new JSONObject(jokeInfo);
+        // If the data contains a joke and a punchline, display both
         if (jokeData.getString("type").equals("twopart")) {
             category = jokeData.getString("category");
             joke = jokeData.getString("setup");
             punchline = jokeData.getString("delivery");
+            printString = "Category:  " + category + "\n " + "\nJoke:  " + joke + "\n " + "\nPunchline:  " + punchline;
         } else {
+            // If the data only contains a joke, display it
             joke = jokeData.getString("joke");
             category = jokeData.getString("category");
-            punchline = " ";
+            printString = "Category:  " + category + "\n " + "\nJoke:  " + joke + "\n ";
         }
-        printString = "Category:  " + category + "\n " + "\nJoke:  " + joke + "\n " +  "\nPunchline:  " + punchline;
-
         return printString;
-
     }
-
-
 }
